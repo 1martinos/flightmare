33ca5d2bff7d31e8cabefc76353129936760036d
1martinos <ucapsm2@ucl.ac.uk>
2022-01-09 14:33:32 -0800

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
+# Make sure conda is installed
+# (I used miniconda)
 if ! command -v conda &> /dev/null
 then
     echo "Please install conda first!"
     exit
 fi
 
-if ! command -v git &> /dev/null
-then
-    echo "Please install git first!"
-    exit
-fi
-
-sudo apt-get update && sudo apt-get install -y --no-install-recommends \ 
+# Install required packages and set up the Bash enviroment for installs and later use
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
      build-essential cmake libzmqpp-dev libopencv-dev
+eval "$(conda shell.bash hook)"
+CUR_DIR=$(dirname "$(readlink -f "$0")")
+FLIGHTMARE_PATH=$CUR_DIR
+echo "export FLIGHTMARE_PATH=$CUR_DIR" >> ~/.bashrc
 
+# Set up conda enviroment
 conda create --name flightmare python=3.6
 conda activate flightmare
-echo "export FLIGHTMARE_PATH=${pwd}" >> ~/.bashrc
 source ~/.bashrc
 
+# Pick which tensorflow depending on whether user has a GPU
 while true; do
     read -p "Do you have a GPU?" yn
     case $yn in
@@ -27,8 +29,12 @@ while true; do
     esac
 done
 pip install scikit-build
+
+# Install packages
 cd flightlib
 pip install .
 
 cd ../flightrl
 pip install .
+
+echo "Installed."
